549974a412bebb4041cb67191cb769fabc7c116a
Ondřej Pešek <iTzBoboCz@users.noreply.github.com>
2021-04-05 00:41:27 +0200

    basic API

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,38 @@
-fn main() {
-    println!("Hello, world!");
+#[macro_use] extern crate diesel;
+extern crate r2d2;
+
+#[allow(unused_imports)]
+use actix_web::{ web, App, HttpServer, Responder, middleware };
+use diesel::SqliteConnection;
+use diesel::r2d2::ConnectionManager;
+
+// mod media;
+// mod errors;
+mod handlers;
+mod models;
+mod schema;
+
+pub type Pool = r2d2::Pool<ConnectionManager<SqliteConnection>>;
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+  dotenv::dotenv().ok();
+  std::env::set_var("RUST_LOG", "actix_web=debug");
+  let database_url = std::env::var("DATABASE_URL").expect("DATABASE_URL must be set");
+
+  // create db connection pool
+  let manager = ConnectionManager::<SqliteConnection>::new(database_url);
+  let pool: Pool = r2d2::Pool::builder()
+  .build(manager)
+  .unwrap();
+
+  HttpServer::new(move || {
+    App::new()
+      .wrap(middleware::Logger::default())
+      .data(pool.clone())
+      .route("/user/username/{name}", web::get().to(handlers::test))
+  })
+  .bind("localhost:3030")?
+  .run()
+  .await
 }
