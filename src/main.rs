402dfab69e66db76697e1bb1469c4cba465201d7
Ondřej Pešek <iTzBoboCz@users.noreply.github.com>
2021-04-15 20:06:03 +0200

    scanning folders

--- src/main.rs
+++ src/main.rs
@@ -12,8 +12,6 @@ extern crate diesel_migrations;
 use actix_web::{ web, App, HttpServer, Responder, middleware };
 use diesel::r2d2::ConnectionManager;
 use diesel::SqliteConnection;
-use infer::{is_audio, is_video};
-use walkdir::WalkDir;
 
 // mod media;
 // mod errors;
@@ -45,38 +43,9 @@ async fn main() -> std::io::Result<()> {
     Err(_) => warn!("Failed to migrate."),
   }
 
-  use std::{fs, io};
-  use std::path::{PathBuf, Path};
-
   let xdg_data = "gallery";
   let username = "ondrejpesek";
-
-  let root = format!("{}/{}/", xdg_data, username);
-
-  for entry in WalkDir::new(&root).into_iter().filter_map(|e| e.ok()) {
-    // https://stackoverflow.com/questions/30309100/how-to-check-if-a-given-path-is-a-file-or-directory#comment105412329_30309566
-    let mut found_folders: Vec<&str>;
-    if entry.path().is_dir() {
-      let string = entry.path().display().to_string();
-      let string_stripped = string.strip_prefix(&root).unwrap();
-
-      if string_stripped == "" { continue };
-
-      let string_split = string_stripped.split("/").map(String::from).collect::<Vec<String>>();
-
-      // skip if folder doesn't contain any pictures or videos
-      if string_split.len() == 1 {
-        if !scan::folder_has_media(PathBuf::from(string)) { continue };
-      }
-
-      warn!("{:?}", string_split);
-      for s in string_split {
-        warn!("{:?}", s);
-        // found_folders.push(s);
-      }
-    }
-    // warn!("{:?}", found_folders);
-  }
+  scan::scan_root(xdg_data, username);
 
   HttpServer::new(move || {
     App::new()
--- src/scan/mod.rs
+++ src/scan/mod.rs
@@ -1,8 +1,8 @@
-use std::{fs, io};
-use std::path::{PathBuf, Path};
+use std::fs;
+use std::path::PathBuf;
 use infer;
 
-pub fn is_media_suppoted(pathbuf: std::path::PathBuf) -> bool {
+pub fn is_media_suppoted(pathbuf: &PathBuf) -> bool {
   let valid_mime_types = [
     "image/jpeg",
     "image/png",
@@ -34,34 +34,68 @@ pub fn is_media_suppoted(pathbuf: std::path::PathBuf) -> bool {
   ];
 
   let kind = infer::get_from_path(pathbuf).unwrap();
-  // let kind = infer::get_from_path(pathbuf.as_path()).unwrap();
 
   if kind.is_none() { return false };
 
-  if !kind.is_none() && valid_mime_types.contains(&kind.unwrap().mime_type()) {
-    warn!("hh{:?}", kind.unwrap().mime_type());
+  if valid_mime_types.contains(&kind.unwrap().mime_type()) {
+    info!("Found: {:?} with type: {:?}", pathbuf, kind.unwrap().mime_type());
+
     return true;
   }
 
   return false;
 }
 
-pub fn folder_has_media(dir: PathBuf) -> bool {
-  let data: Vec<PathBuf> = fs::read_dir(&dir).unwrap()
+pub fn scan_recursively(path: PathBuf) -> Vec<PathBuf> {
+  // skip empty folders
+  if path.read_dir().map(|mut i| i.next().is_none()).unwrap_or(false) { return Vec::new() }
+
+  let folders = fs::read_dir(path.clone()).unwrap()
     .into_iter()
     .filter(|r| r.is_ok()) // Get rid of Err variants for Result<DirEntry>
     .map(|r| r.unwrap().path()) // This is safe, since we only have the Ok variants
-    .filter(|r| r.is_file()) // Get rid of Err variants for Result<DirEntry>
-    .filter(|r| is_media_suppoted(PathBuf::from(r)) == false) // Filter out non-folders
-    .collect();
+    .filter(|r| r.is_dir())
+    .collect::<Vec<PathBuf>>();
 
-  for x in &data {
-    warn!("{:?}", x);
+  let mut array: Vec<PathBuf> = Vec::new();
+  for folder in folders.clone() {
+    array.append(&mut scan_recursively(folder));
   }
 
-  if data.len() > 0 {
-    return true;
+  let files = fs::read_dir(path.clone()).unwrap()
+    .into_iter()
+    .filter(|r| r.is_ok()) // Get rid of Err variants for Result<DirEntry>
+    .map(|r| r.unwrap().path()) // This is safe, since we only have the Ok variants
+    .filter(|r| r.is_file())
+    .filter(|r| is_media_suppoted(r))
+    .collect::<Vec<PathBuf>>();
+
+  if files.len() > 0 {
+    array.push(path);
   }
 
-  return false;
+  return array;
 }
+
+pub fn scan_root(xdg_data: &str, username: &str) {
+  // root directory
+  let current_dir = format!("{}/{}/", xdg_data, username);
+
+  let mut found_folders: Vec<PathBuf> = Vec::new();
+
+  info!("Scanning files and folders for user {} started.", username);
+
+  let folders = fs::read_dir(current_dir.clone()).unwrap()
+    .into_iter()
+    .filter(|r| r.is_ok()) // Get rid of Err variants for Result<DirEntry>
+    .map(|r| r.unwrap().path()) // This is safe, since we only have the Ok variants
+    .collect::<Vec<PathBuf>>();
+
+  if folders.len() > 0 {
+    found_folders.append(&mut scan_recursively(PathBuf::from(current_dir)));
+  }
+
+  debug!("{:?}", found_folders);
+  info!("Scanning is done.");
+}
+
