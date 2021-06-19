f543ec1581c3ed2fdc78e035fc74eb08831d566e
Ondřej Pešek <iTzBoboCz@users.noreply.github.com>
2021-06-19 12:35:16 +0200

    favorite_media in schema.rs

--- src/schema.rs
+++ src/schema.rs
@@ -18,6 +18,14 @@ table! {
 }
 
 table! {
+  favorite_media (id) {
+    id -> Integer,
+    media_id -> Integer,
+    user_id -> Integer,
+  }
+}
+
+table! {
   folder (id) {
     id -> Integer,
     owner_id -> Integer,
@@ -37,7 +45,6 @@ table! {
     height -> Integer,
     date_taken -> Timestamp,
     sha2_512 -> Varchar,
-    favorite -> Bool,
   }
 }
 
@@ -52,6 +59,8 @@ table! {
 joinable!(album -> user (owner_id));
 joinable!(album_invite -> album (album_id));
 joinable!(album_invite -> user (invited_user_id));
+joinable!(favorite_media -> media (media_id));
+joinable!(favorite_media -> user (user_id));
 joinable!(folder -> user (owner_id));
 joinable!(media -> album (album_id));
 joinable!(media -> folder (folder_id));
@@ -60,6 +69,7 @@ joinable!(media -> user (owner_id));
 allow_tables_to_appear_in_same_query!(
   album,
   album_invite,
+  favorite_media,
   folder,
   media,
   user,
