03b19e7691fcc636d573a8496931ba19b31ed3a3
Ondřej Pešek <iTzBoboCz@users.noreply.github.com>
2022-09-21 00:08:20 +0200

    fix: make more database queries work with new pool

--- src/db/users.rs
+++ src/db/users.rs
@@ -18,13 +18,13 @@ use diesel::Table;
 /// };
 /// insert_user(&conn, user);
 /// ```
-pub async fn insert_user(conn: &DbConn, user: NewUser) -> usize {
-  conn.run(move |c| {
+pub async fn insert_user(conn: DbConn, user: NewUser) -> usize {
+  conn.interact(move |c| {
     diesel::insert_into(user::table)
       .values(user.clone())
       .execute(c)
       .unwrap_or_else(|_| panic!("Error creating user {}", user.username))
-  }).await
+  }).await.unwrap()
 }
 
 /// Checks whether user is unique in database.
@@ -40,8 +40,8 @@ pub async fn insert_user(conn: &DbConn, user: NewUser) -> usize {
 ///   insert_user(&conn, user);
 /// }
 /// ```
-pub async fn is_user_unique(conn: &DbConn, user: NewUser) -> bool {
-  conn.run(move |c| {
+pub async fn is_user_unique(conn: DbConn, user: NewUser) -> bool {
+  conn.interact(move |c| {
     let user_id: Option<i32> = user::table
       .select(user::id)
       .filter(user::username.eq(user.username))
@@ -53,7 +53,7 @@ pub async fn is_user_unique(conn: &DbConn, user: NewUser) -> bool {
     if user_id.is_some() { return false }
 
     true
-  }).await
+  }).await.unwrap()
 }
 
 /// Gets user's ID from username.
@@ -62,15 +62,15 @@ pub async fn is_user_unique(conn: &DbConn, user: NewUser) -> bool {
 /// ```
 /// let user: Option<i32> = get_user_id(&conn, String::from("michael"));
 /// ```
-pub async fn get_user_id(conn: &DbConn, username: String) -> Option<i32> {
-  conn.run(move |c| {
+pub async fn get_user_id(conn: DbConn, username: String) -> Option<i32> {
+  conn.interact(move |c| {
     user::table
       .select(user::id)
       .filter(user::username.eq(username))
       .first(c)
       .optional()
       .unwrap()
-  }).await
+  }).await.unwrap()
 }
 
 /// Tries to select a user by its ID.
@@ -79,61 +79,61 @@ pub async fn get_user_id(conn: &DbConn, username: String) -> Option<i32> {
 /// ```
 /// let username: Option<String> = get_user_username(&conn, 1);
 /// ```
-pub async fn get_user_username(conn: &DbConn, user_id: i32) -> Option<String> {
-  conn.run(move |c| {
+pub async fn get_user_username(conn: DbConn, user_id: i32) -> Option<String> {
+  conn.interact(move |c| {
     user::table
       .select(user::username)
       .filter(user::id.eq(user_id))
       .first(c)
       .optional()
       .unwrap()
-  }).await
+  }).await.unwrap()
 }
 
 /// Tries to select a user by its ID.
-pub async fn get_user_by_id(conn: &DbConn, user_id: i32) -> Option<User> {
-  conn.run(move |c| {
+pub async fn get_user_by_id(conn: DbConn, user_id: i32) -> Option<User> {
+  conn.interact(move |c| {
     user::table
       .select(user::table::all_columns())
       .filter(user::id.eq(user_id))
       .first::<User>(c)
       .optional()
       .unwrap()
-  }).await
+  }).await.unwrap()
 }
 
 /// Tries to select a user ID from a given email.
-pub async fn get_user_id_email(conn: &DbConn, email: String) -> Option<i32> {
-  conn.run(move |c| {
+pub async fn get_user_id_email(conn: DbConn, email: String) -> Option<i32> {
+  conn.interact(move |c| {
     user::table
       .select(user::id)
       .filter(user::email.eq(email))
       .first(c)
       .optional()
       .unwrap()
-  }).await
+  }).await.unwrap()
 }
 
 /// Checks the database for a combination of a specified username and password.
-pub async fn check_user_login_username(conn: &DbConn, username: String, password: String) -> Option<i32> {
-  conn.run(move |c| {
+pub async fn check_user_login_username(conn: DbConn, username: String, password: String) -> Option<i32> {
+  conn.interact(move |c| {
     user::table
       .select(user::id)
       .filter(user::username.eq(username).and(user::password.eq(password)))
       .first(c)
       .optional()
       .unwrap()
-  }).await
+  }).await.unwrap()
 }
 
 /// Checks the database for a combination of a specified email and password.
-pub async fn check_user_login_email(conn: &DbConn, email: String, password: String) -> Option<i32> {
-  conn.run(move |c| {
+pub async fn check_user_login_email(conn: DbConn, email: String, password: String) -> Option<i32> {
+  conn.interact(move |c| {
     user::table
       .select(user::id)
       .filter(user::email.eq(email).and(user::password.eq(password)))
       .first(c)
       .optional()
       .unwrap()
-  }).await
+  }).await.unwrap()
 }
