98f5bdb1ce20a1ff99cadde38ffa3b5d0ae24dda
Ondřej Pešek <iTzBoboCz@users.noreply.github.com>
2026-01-10 23:57:56 +0100

    fix: wrong defaults for tracing

--- src/main.rs
+++ src/main.rs
@@ -61,10 +61,10 @@ pub struct AppState {
 #[tokio::main]
 async fn main() {
   tracing_subscriber::registry()
-    .with(tracing_subscriber::EnvFilter::new(
-        std::env::var("RUST_LOG")
-          .unwrap_or_else(|_| "example_tracing_aka_logging=debug,tower_http=debug".into()),
-    ))
+    .with(
+      tracing_subscriber::EnvFilter::try_from_default_env()
+      .unwrap_or_else(|_| "info,tower_http=debug".into()),
+    )
     .with(tracing_subscriber::fmt::layer())
     .init();
 
